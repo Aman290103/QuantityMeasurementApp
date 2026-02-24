dbe78c670dec538289755423fad301d6e4503dcf
Aman290103 <amankumarchaubey2901@gmail.com>
2026-02-24 08:56:35 +0530

    "[ Aman Kumar Chaubey ] Add. Refactor feature/UC4-Extended Units"

--- QuantityMeasurementApp.Core/LengthUnit.cs
+++ QuantityMeasurementApp.Core/LengthUnit.cs
@@ -11,32 +11,22 @@ namespace QuantityMeasurementApp.Core
 
     public static class LengthUnitExtensions
     {
-        /// <summary>
-        /// Returns the value converted to the base unit (Inches).
-        /// </summary>
         public static double GetBaseValue(this LengthUnit unit, double value)
         {
-            switch (unit)
+            if (unit == LengthUnit.Feet)
             {
-                case LengthUnit.Feet:
-                    return value * 12.0;
-                case LengthUnit.Yard:
-                    return value * 36.0;
-                case LengthUnit.Centimeter:
-                    return value * 0.393701;
-                case LengthUnit.Inch:
-                    return value * 1.0;
-                default:
-                    return value;
+                return value * 12.0;
             }
-        }
-
-        /// <summary>
-        /// Gets the conversion factor relative to the base unit (Inches).
-        /// </summary>
-        public static double GetConversionFactor(this LengthUnit unit)
-        {
-            return unit.GetBaseValue(1.0);
+            if (unit == LengthUnit.Yard)
+            {
+                return value * 36.0;
+            }
+            if (unit == LengthUnit.Centimeter)
+            {
+                return value * 0.393701;
+            }
+            
+            return value; 
         }
     }
 }
